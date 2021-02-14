de623583625958148f7a82e7e9f6b1373e81f448
ExeVirus <ddsallad@gmail.com>
2021-02-14 16:16:58 -0500

    Coffin added, 18% done

--- init.lua
+++ init.lua
@@ -56,10 +56,31 @@ node_definition ={
 }
 autobox.register_node("graveyard_pack:border_pillar","border_pillar.box",node_definition,true)
 adv_core.register_object("graveyard_pack:border_pillar", 2, 0, 2, 0)
--- brick_wall
--- brickWallCurve
--- coffin
--- columnLarge
+
+node_definition ={
+	description = "graveyard_pack:coffin",
+	drawtype = "mesh",
+	mesh = "coffin.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"coffin.png"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("graveyard_pack:coffin","coffin.box",node_definition,true)
+adv_core.register_object("graveyard_pack:coffin", 2, 0, 2, 0)
+
+-- node_definition ={
+	-- description = "graveyard_pack:column",
+	-- drawtype = "mesh",
+	-- mesh = "column.obj",
+	-- sunlight_propagates = true,
+	-- paramtype2 = "facedir",
+	-- tiles = {"column.png"},
+	-- groups = { oddly_breakable_by_hand=2 },
+-- }
+-- autobox.register_node("graveyard_pack:column","column.box",node_definition,true)
+-- adv_core.register_object("graveyard_pack:column", 2, 0, 2, 0)
+
 -- cross
 -- crossColumn
 -- crypt
